2568184931b3f60c739ffbd96f25e003c11558df
DESKTOP-2GE40N5\bright <qupelor21@gmail.com>
2018-04-25 00:53:55 +0700

    อะ

--- backend/views/about/view.php
+++ backend/views/about/view.php
@@ -10,11 +10,9 @@ $this->title = $model->about_id;
 $this->params['breadcrumbs'][] = ['label' => 'Abouts', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="box box-success box-solid">
-    <div class="box-header">
+
         <h1 class="box-title"><?= Html::encode($this->title) ?></h1>
-    </div>
-    <div class="box-body">
+
         <p>
         <?= Html::a('แก้ไข', ['update', 'id' => $model->about_id], ['class' => 'btn btn-primary']) ?>
         
@@ -31,5 +29,3 @@ $this->params['breadcrumbs'][] = $this->title;
             'about_view',
         ],
     ]) ?>
-    </div>
-</div>
--- backend/views/banner/view.php
+++ backend/views/banner/view.php
@@ -10,12 +10,8 @@ $this->title = $model->ban_id;
 $this->params['breadcrumbs'][] = ['label' => 'Banners', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="box box-primary box-solid">
-    <div class="box-header">
-        <h1 class="box-title"><?= Html::encode($this->title) ?></h1>
-    </div>
 
-    <div class="box-body">
+
     <p>
         <?= Html::a('แก้ไข', ['update', 'id' => $model->ban_id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('ลบ', ['delete', 'id' => $model->ban_id], [
@@ -43,5 +39,3 @@ $this->params['breadcrumbs'][] = $this->title;
             'ban_detail:ntext',
         ],
     ]) ?>
-    </div>
-</div>
